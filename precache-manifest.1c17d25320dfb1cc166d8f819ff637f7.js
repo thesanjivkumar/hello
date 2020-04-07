ae1959694f66a2a09d181e2cee7ad6ff63f02d52
SANJIV KUMAR <sanjivdev@SANJIVs-MacBook-Pro.local>
2020-04-07 10:12:41 +0530

    Updates

---
+++ precache-manifest.1c17d25320dfb1cc166d8f819ff637f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48b8d795b85c89dd0755103cb29752f",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "77379788d48855724b8d",
+    "url": "/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c46a6f6f8c4e849cc9a7",
+    "url": "/hello/static/js/2.b6a74097.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.b6a74097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77379788d48855724b8d",
+    "url": "/hello/static/js/main.464ef8db.chunk.js"
+  },
+  {
+    "revision": "0446d958df867e2378ee",
+    "url": "/hello/static/js/runtime-main.bd64abc2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
